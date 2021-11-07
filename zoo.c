7e0fd80447445d73174133ad244541bededab00f
ishmeet07 <84372705+ishmeet07@users.noreply.github.com>
2021-11-07 22:23:20 +0530

    Create zoo.c

---
+++ zoo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	char num[20];
+    int k=0,l=0,i; 
+    
+	scanf("%s", &num);    
+	for(i=0;i<20;i++)
+	   {           
+                if(num[i]=='z'||num[i]=="Z")
+                    k++;
+	            else if(num[i]=='o'||num[i]=="O")
+	                l++;
+       }
+	k=(2*k);
+        if(l==k)
+	        printf("Yes");
+        else
+            printf("No");
+}
