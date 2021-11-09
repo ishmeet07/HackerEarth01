1c43c94738c1c11e3dde2ba0dad4bb1f01b2b54a
ishmeet07 <84372705+ishmeet07@users.noreply.github.com>
2021-11-09 22:10:58 +0530

    Create splithouses.c
    SPLIT HOUSES HACKER EARTH
    https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/split-house-547be0e9/

---
+++ splithouses.c
@@ -0,0 +1,39 @@
+
+// split houses hacker earth
+#include <stdio.h>
+
+int main(){
+	int num;
+	scanf("%d", &num);              			// Reading input from STDIN
+
+	char arr[50];
+	
+	for(int i=0;i<num+1;i++)   
+	scanf("%c",&arr[i]);
+	for(int i=0;i<num+1;i++)  
+	  { if(arr[i]=='H'&&i==0)
+	        arr[i+1]='B';
+         else if(arr[i]=='.') 
+	        arr[i]='B';
+	  }
+	int k=0,l=0;
+	for(int i=0;i<num;i++)  
+	 {  if(arr[i]=='H'&&arr[i+1]=='H')
+	      {l=1;
+		  }
+		 else   
+		   {
+			 
+			 k=1;
+		   }
+	 }
+	
+	 if(l!=0)
+	 printf("NO");
+	 else if(k!=0)
+	{     printf("YES\n");
+		for(int i=0;i<num+1;i++) 
+	       {printf("%c",arr[i]); }  
+	 }
+   
+}
