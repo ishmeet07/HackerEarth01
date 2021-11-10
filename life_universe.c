087c92e9fb6554c10cfe4b02a3930600837a23fc
ishmeet07 <84372705+ishmeet07@users.noreply.github.com>
2021-11-10 23:38:57 +0530

    Create life_universe.c
    Life, the Universe, and Everything(HACKER EARTH )(easy)
    https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/life-the-universe-and-everything/

---
+++ life_universe.c
@@ -0,0 +1,20 @@
+//this is made by ishmeet singh
+//Life, the Universe, and Everything(HACKER EARTH)
+#include<stdio.h>
+int main()
+{ int a[10000],c=0;
+ for(int i=0;i<10000;i++)
+scanf("%d",&a[i]);
+      for(int i=0;i<10000;i++)
+  	{ 	
+		  if(a[i]!=42)
+      		{printf("%d\n",a[i]);
+   			
+   			}
+    	  else   
+   			{break;
+   			}
+    }
+  
+  
+}
