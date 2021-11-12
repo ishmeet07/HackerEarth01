e6089ca962637e5d539fe1d83f96161c4ea0abb0
ishmeet07 <84372705+ishmeet07@users.noreply.github.com>
2021-11-12 22:51:09 +0530

    Create factorial.c
    factorial hacker earth(easy)
    https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/find-factorial/

---
+++ factorial.c
@@ -0,0 +1,13 @@
+
+//this is made by ishmeet singh
+//factorial hacker earth
+#include <stdio.h>
+
+int main(){
+	int num,k=1;
+	scanf("%d", &num); 
+	for(int i=1;i<=num;i++)
+	    {  k=k*i;}        			
+	printf("%d",k);       // Writing output to STDOUT
+}
+
